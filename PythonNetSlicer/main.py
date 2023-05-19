ed5330839f612308af9780649e1e98cfafcf86c0
ahmedalkinanimo <aaljanabi@kckcc.edu>
2023-05-19 11:12:35 -0500

    testing

--- PythonNetSlicer/main.py
+++ PythonNetSlicer/main.py
@@ -1,6 +1,16 @@
 
+'''
+The primary aim of this project is to develop an application that can subnet a 
+given IPv4 address using three different subnetting methods. Additionally, the 
+application validates the input IPv4 address and computes the corresponding network 
+IPv4 address. For each network, the application determines the first, last, and 
+broadcast IPv4 addresses and also computes the number of available usable IP 
+addresses. The goal of this project is to accomplish these tasks without relying on 
+any pre-existing network libraries.
+Strategic Pattern Design will be utilized in this project.
 
+'''
 
 if __name__ == "__main__":
-    print("test")
+    print("NetSlicer")
     
