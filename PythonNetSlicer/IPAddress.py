efabbb15e0df8741eca45fc17a6fecad322d9f9b
ahmedalkinanimo <aaljanabi@kckcc.edu>
2023-05-22 10:11:39 -0500

    update 5/22/2023

--- PythonNetSlicer/IPAddress.py
+++ PythonNetSlicer/IPAddress.py
@@ -1,79 +1,5 @@
 
-'''
-public class networkIPAddress{
-		
-	public void setNetIp() { 
-		// this method will find the network IP address: in a String form and numerical form of 4 octets
-		//  Network IP address = Entered IP address & (bitwise AND) subnet mask
-		this.netIp="";
-		String temp;
-		for(int i=0;i<4;i++) {
-			this.netIpOctet[i]=this.enteredIp.getIpOctet()[i]&this.enteredIp.getSubnetMask()[i];
-			temp=String.valueOf(this.netIpOctet[i]);
-			this.netIp+=(i!=3?temp+".":temp);
-		}
-	}
-	
-	
 
-	public void setFirstIpAddress() {
-		// this method will find the 1st available IP address: 
-		//  1st available IP address = netIP+1
-		this.firstIpAddress="";
-		for(int i=0;i<4;i++) {
-			firstIpAddress+=(i!=3?String.valueOf(this.netIpOctet[i])+".":String.valueOf(this.netIpOctet[i]+1));
-		}
-	}
-
-	public void setLastIpAddress() {
-		// this method will find the last IP address:
-		// last IP address= [netIP ~ (bitwise negation) of subnet mask] -1
-		// integers are represented using two's complement notation in Java. 
-		// To convert the bitwise negation of 255 back to its unsigned binary representation, 
-		// we added 256 to the result
-		this.lastIpAddress="";
-		for(int i=0;i<4;i++) {
-			lastIpAddress+=(i!=3?String.valueOf(netIpOctet[i]|(~enteredIp.getSubnetMask()[i]+256))+".":String.valueOf(netIpOctet[i]|(~enteredIp.getSubnetMask()[i]+256)-1));
-		}
-	}
-
-	public void setbroadCastIpAddress() {
-		// this method will find the broad cast IP address:
-		// broad cast IP address= netIP ~ (bitwise negation) of subnet mask
-		// integers are represented using two's complement notation in Java. 
-		// To convert the bitwise negation of 255 back to its unsigned binary representation, 
-		// we added 256 to the result
-		this.broadCastIpAddress="";
-		for(int i=0;i<4;i++) {
-			broadCastIpAddress+=(i!=3?String.valueOf(netIpOctet[i]|(~enteredIp.getSubnetMask()[i]+256))+".":String.valueOf(netIpOctet[i]|(~enteredIp.getSubnetMask()[i]+256)));
-		}
-	}
-
-	public void setNumberOfHosts() {
-		// the size of host portion of the ip address = 32- prefix
-		// # of usable hosts = 2 to the power of (32- prefix) -2
-		this.numberOfHosts=(int)Math.pow(2,(32-this.enteredIp.getPrefix()))-2;
-	}
-	
-	public String toString() {
-		// returns a string representation of an object (Network IP address) 
-		String str="Entered IP address: "+this.getEnteredIp();
-		str+="\nNetwork Ip address :"+this.netIp+"/"+this.getPrefix();
-		str+="\nSubnet mask: "+this.enteredIp.getSubnetMaskStr();
-		str+="\nNumber of Hosts :"+this.numberOfHosts;
-		str+="\nBroad Cast Ip Address :"+this.broadCastIpAddress;
-		str+="\nFirst Ip Address :"+this.firstIpAddress;
-		str+="\nLast Ip Address :"+this.lastIpAddress;
-		return str;
-	}
-	
-	public void setNetIpOctet(int netIpOctet, int ind) {
-		this.netIpOctet[ind] = netIpOctet;
-	}
-    
-	
-}
-'''
 #----------------------------------------------------------------------
 class IPAddress:
 
@@ -141,7 +67,7 @@ class IPAddress:
         
         # Generating a sequence of 32 ones and shifting them to the left by (32 - prefix)
         # Example: if the prefix is 24, the 32 ones will be shifted to the left by 8, resulting in 24 ones assigned to the mask
-        mask = 0xFFFFFFFF << (32 - self.prefix)
+        mask = 0xFFFFFFFF << (32 - self._prefix)
 
         # Breaking down the mask into 4 octets of the subnet mask and generating the string form of the subnet mask
         step_size = 24
@@ -151,7 +77,7 @@ class IPAddress:
             # bitwise AND operation will be performed with 0xFF (11111111) to find the octets
             self._subnet_mask[i] = (mask >> step_size) & 0xFF
             step_size -= 8
-            self._subnet_mask_str += str(self.subnet_mask[i]) + ("." if i != 3 else "")
+            self._subnet_mask_str += str(self._subnet_mask[i]) + ("." if i != 3 else "")
 
     def get_Ip(self)->str:
         return self._ipAddress
@@ -184,7 +110,7 @@ class NetworkIpAddress:
         self._numberOfHosts = 0
 
         # Set the entered IP
-        self.set_entered_Ip(enteredIp)
+        self.set_entered_Ip(ip)
 
         # Set the network IP, the network Ip Octets & network prefix
         self.set_netIp()
@@ -202,53 +128,61 @@ class NetworkIpAddress:
         self.set_number_of_hosts()
 
 
-    def __str__(self)->str:
-        pass
+    def __str__(self)->str: 
+        return "Net IP: "+self._netIp+"/"+str(self._netPrefix)+"\n1st IP address: "+self._firstIpAddress+"\nlast IP address: "+self._lastIpAddress+"\nBroad Cast IP address: "+self._broadCastIpAddress+"\n# usable IP addresses: "+str(self._numberOfHosts)
 
     # getter methods
-    def get_entered_Ip()-> str:
+    def get_entered_Ip(self)-> str:
         return self._enteredIp
 
-    def get_netIp()-> str:
+    def get_netIp(self)-> str:
         return self._netIp
 
-    def get_first_IpAddress()-> str:
+    def get_first_IpAddress(self)-> str:
         return self._firstIpAddress
 
-    def get_last_IpAddress()-> str:
+    def get_last_IpAddress(self)-> str:
         return self._lastIpAddress
 
-    def get_broadCast_IpAddress()->str:
+    def get_broadCast_IpAddress(self)->str:
         return self._broadCastIpAddress
 
-    def get_number_of_hosts()-> int:
+    def get_number_of_hosts(self)-> int:
         return self._numberOfHosts
 
-    def get_netPrefix()-> int:
+    def get_netPrefix(self)-> int:
         return self._netPrefix
 
     # setter methods
-    def set_entered_Ip(ip: IPAddress):
+    def set_entered_Ip(self, ip: IPAddress):
         self._enteredIp = ip
         
-    def set_netIp():
+    def set_netIp(self):
         self._netPrefix = self._enteredIp.get_Prefix()
         tempStr= ""
         for i in range(4):
             self._netIpOctet[i]=self._enteredIp.get_IpOctets()[i] & self._enteredIp.get_subnetMaskOctets()[i]
-            self._netIp+=str(this.netIpOctet[i])+"." if i!=3 else str(this.netIpOctet[i])
-    
+            self._netIp+=str(self._netIpOctet[i])+"." if i!=3 else str(self._netIpOctet[i])
 
-    def set_first_IpAddress():
-        pass
+    def set_first_IpAddress(self):
+        for i in range(4):
+            self._firstIpAddress+=str(self._netIpOctet[i])+"." if i!=3 else str(self._netIpOctet[i]+1)
 
-    def set_last_IpAddress():
-        pass
+    def set_last_IpAddress(self):
+        for i in range(4):
+            #x1=self._netIpOctet[i]
+            #x2=-(x1 ^ ((1<<x1.bit_length())-1))+255
+            #self._lastIpAddress+=str(x1 | x2)+"." if i!=3 else str((x1 | x2)-1)
+            pass
 
-    def set_broadCast_IpAddress():
-        pass
-
-    def set_number_of_hosts():
-        pass
+    def set_broadCast_IpAddress(self):
+        for i in range(4):         
+            #x1 = self._netIpOctet[i]
+            #x2 = 255-x1
+            #self._broadCastIpAddress += str(x1 + x2) + "." if i != 3 else str(x1 + x2)
+            pass
+            
+    def set_number_of_hosts(self):
+        self._numberOfHosts=int(2**(32-self._netPrefix)-2)
      
 #---------------------------------------------------------------------------------------
